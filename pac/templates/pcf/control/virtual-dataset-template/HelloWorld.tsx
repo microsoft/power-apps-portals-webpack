a42c3b33a8dab2bb7db6df0a6563d59a704f19dd
melody-universe <tyler.hogsett@microsoft.com>
2021-06-23 19:27:20 -0700

    removed pac

--- pac/templates/pcf/control/virtual-dataset-template/HelloWorld.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:67269d0085d0387c395a5b1f18914b61b87e9e7689333536bc8ab8af4ee63220
-size 219
--- pac/templates/plugin/template_Plugin1.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ae9f3beab2ce1e5b1b82f9517e5b382222f45493baad6c3a19c90adcbc80dedd
-size 1642
