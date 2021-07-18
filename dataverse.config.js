2adcc0e10a021f015bd9e5b6e5eca380f8c77808
melody-universe <tyler.hogsett@microsoft.com>
2021-07-17 21:30:37 -0700

    redesigned handle mapping based on exported portal config

--- dataverse.config.js
+++ dataverse.config.js
@@ -1,16 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
-  environmentUrl: "https://mel-portal-dev-github.crm.dynamics.com/",
+  portalPath: "portal",
+  environmentUrl: process.env.URL,
   credentials: {
-    clientId: "819b7c55-277e-43cc-b062-64d00285f44d",
-    clientSecret: "6oWzo05-1nUfkg2_Ya--U2CD~gOAXDGCL2",
-    authority: "https://login.microsoftonline.com/",
-    tenantId: "1557f771-4c8e-4dbd-8b80-dd00a88e833e",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    authority: process.env.AUTHORITY,
+    tenantId: process.env.TENANT_ID,
   },
   assets: {
-    "main.js": {
-      entityLogicalName: "adx_entityform",
-      contentAttribute: "adx_registerstartupscript",
-      id: "eb7a2255-abd4-eb11-bacc-00224820e6a0",
-    },
+    "main.js": "starter-portal/basic-forms/new-ticket",
   },
 };
