afd508bff90810ca6bce139993b49b799fb737b5
melody-universe <tyler.hogsett@microsoft.com>
2021-07-19 20:43:02 -0700

    removed webpack-generated assets

--- portal/starter-portal/advanced-forms/application/advanced-form-steps/01---personal-information/01---Personal-Information.advancedformstep.custom_javascript.js
+++
@@ -1,3 +0,0 @@
-const scriptElement = document.createElement('script');
-scriptElement.src = "/adx_webformstep_bceefe80-f5e7-eb11-bacb-000d3a1b79d2.js";
-document.body.appendChild(scriptElement);
--- portal/starter-portal/basic-forms/new-ticket/New-ticket.basicform.custom_javascript.js
+++
@@ -1,3 +0,0 @@
-const scriptElement = document.createElement('script');
-scriptElement.src = "/adx_entityform_eb7a2255-abd4-eb11-bacc-00224820e6a0.js";
-document.body.appendChild(scriptElement);
